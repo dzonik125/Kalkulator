55baf81327c12ffaf96325f2a943f8685940e32f
Nikola Kolarov <dzonik125@gmail.com>
2021-10-18 14:07:09 +0200

    initial read-loop

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,10 +1,19 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner s1 = new Scanner(System.in);
+		while(true) {
+			String a;
+			a = s1.nextLine();
+			if(a.equals("exit") || a.equals("kraj")) {
+				break;
+			}
+		}
 	}
 
 }
